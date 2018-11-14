434fced6ef227f121a48fb7a0b3d95f9009e32b1
Sungchan Yi <38686321+calofmijuck@users.noreply.github.com>
2018-11-15 08:53:13 +0900

    Update 16394.cpp

--- Codes/16300/16394.cpp
+++ Codes/16300/16394.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int var;
-	cin >> var;
-	cout << var - 1946;
-	return 0;
+    cin.tie(NULL);
+    int var;
+    cin >> var;
+    cout << var - 1946;
+    return 0;
 }
