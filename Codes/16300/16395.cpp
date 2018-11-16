37019f730adf1b210c79ba3b2eaabcf138c8e107
Sungchan Yi <calofmijuck@snu.ac.kr>
2018-11-16 14:18:02 +0900

    Update #16395

--- Codes/16300/16395.cpp
+++ Codes/16300/16395.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-	int n, k;
-	scanf("%d %d", &n, &k);
-	long long prod = 1;
-	for(int i = 1; i <= k - 1; ++i)
-		prod = prod * (n - i) / i;
-	printf("%lld", prod);
-	return 0;
+    int n, k;
+    scanf("%d %d", &n, &k);
+    long long prod = 1;
+    for(int i = 1; i <= k - 1; ++i)
+        prod = prod * (n - i) / i;
+    printf("%lld", prod);
+    return 0;
 }
