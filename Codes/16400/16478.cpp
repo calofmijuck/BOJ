c28d14f9a0c1884e9c02993548f8940077d8438c
Sungchan Yi <calofmijuck@snu.ac.kr>
2018-11-18 10:56:12 +0900

    #16478 Accepted!

---
+++ Codes/16400/16478.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long a, b, c, d;
+    scanf("%lld %lld %lld", &a, &b, &c);
+    printf("%lf", (double) a * c / b);
+    return 0;
+}
