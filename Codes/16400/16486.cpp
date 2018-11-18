8ce58cdf8ada33f0b69721a114468a6acdb0303b
Sungchan Yi <calofmijuck@snu.ac.kr>
2018-11-18 11:02:13 +0900

    #16486 Accepted!

---
+++ Codes/16400/16486.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.141592
+
+int main() {
+    int d1, d2;
+    scanf("%d %d", &d1, &d2);
+    printf("%lf", PI * 2 * d2 + 2 * d1);
+    return 0;
+}
