68b7b3a87703c146a902254b7913511bf76b607a
Sungchan Yi <calofmijuck@snu.ac.kr>
2018-12-04 11:15:36 +0900

    Test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool testPrime(int n) {
+    for(int i = 2; i * i <= n; ++i) {
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
-    printf("%d", -1 % 5);
+    for(int i = 2; i <= 100; ++i) {
+        if(testPrime(i)) printf("%d ", i);
+    }
+
     return 0;
 }
