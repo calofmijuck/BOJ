9a90bc0e9e12bb0e8ef3f607512185da04994f1a
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-02-14 18:53:43 +0900

    #2470 Accepted!

---
+++ Codes/2400/2470.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 2147483647
+
+int arr[101010], mi = MAX, res[2];
+
+bool cmp(int a, int b) {
+    return abs(a) > abs(b);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; ++i) scanf("%d", arr + i);
+    sort(arr, arr + n, cmp);
+    for(int i = 0; i < n - 1; ++i) {
+        if(abs(arr[i] + arr[i + 1]) < mi) {
+            mi = abs(arr[i] + arr[i + 1]);
+            res[0] = arr[i];
+            res[1] = arr[i + 1];
+        }
+    }
+    sort(res, res + 2);
+    printf("%d %d", res[0], res[1]);
+    return 0;
+}
