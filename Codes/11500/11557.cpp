ef898e7621a44929993f1b1baec87a2d53b65b0d
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-03-02 11:19:40 +0900

    #11557 Accepted!

---
+++ Codes/11500/11557.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, n, mx, in;
+    cin >> t;
+    string str, ans;
+    while(t--) {
+        cin >> n;
+        mx = 0;
+        for(int i = 0; i < n; ++i) {
+            cin >> str >> in;
+            if(in > mx) {
+                mx = in;
+                ans = str;
+            }
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
