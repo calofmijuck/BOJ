d78504d9808834411d6fc0043fe304a08430f508
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-06-03 03:26:44 +0900

    Resolve #9251

--- Codes/9200/9251.cpp
+++ Codes/9200/9251.cpp
@@ -9,7 +9,8 @@ int main() {
     int sa = a.size(), sb = b.size();
     for(int i = 1; i <= sa; ++i) {
         for(int j = 1; j <= sb; ++j)
-            dp[i][j] = max({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] + (a.at(i - 1) == b.at(j - 1))});
+            if(a[i - 1] == b[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
     }
     printf("%d", dp[sa][sb]);
     return 0;
