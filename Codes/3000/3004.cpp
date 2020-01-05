e074732912e9b8722e2b133dfbd7f62c5a1f801a
Sungchan Yi <calofmijuck@snu.ac.kr>
2020-01-05 22:59:41 +0900

    #3004 Accepted!

---
+++ Codes/3000/3004.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    n += 2;
+    cout << n * n / 4;
+    return 0;
+}
