172ed7ebb2cbff4929c0d2f4413926b49af5d4fb
Sungchan Yi <calofmijuck@snu.ac.kr>
2020-01-05 23:00:12 +0900

    #3003 Accepted!

---
+++ Codes/3000/3003.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int king, queen, rook, bishop, knight, pawn;
+    scanf("%d%d%d%d%d%d", &king, &queen, &rook, &bishop, &knight, &pawn);
+    printf("%d %d %d %d %d %d", 1 - king, 1 - queen, 2 - rook, 2 - bishop, 2 - knight, 8 - pawn);
+    return 0;
+}
