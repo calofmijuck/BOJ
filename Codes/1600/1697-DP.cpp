9b688b90bf2aab9d6b73c4ad8de74534b9b6ce05
calofmijuck <calofmijuck@snu.ac.kr>
2020-06-18 22:48:16 +0900

    #1697 Updated.

--- Codes/1600/1697-DP.cpp
+++ Codes/1600/1697-DP.cpp
@@ -6,18 +6,12 @@ int n, k, d[100001];
 
 int main() {
 	scanf("%d %d", &n, &k);
-	if(n > k) {
-		printf("%d", n - k);
-		return 0;
-	}
-	if(n == 0) d[n + 1] = 1;
-	else d[n - 1] = d[n + 1] = 1;
-    int idx = n - 2, tmp = 2;
+    int idx = n, tmp = 0;
     while(idx >= 0) {
         d[idx] = tmp;
         idx--, tmp++;
     }
-	for(int i = n + 2; i <= k + 1; i++) {
+	for(int i = n + 1; i <= k + 1; i++) {
 		if(i % 2 == 0) d[i] = min(d[i - 1], d[i / 2]) + 1;
 		else d[i] = d[i - 1] + 1;
 		d[i - 1] = min(d[i - 1], d[i] + 1);
