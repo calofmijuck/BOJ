77361c7384d9f5041a40d2ad59cc01391f7ced5f
Sungchan Yi <calofmijuck@snu.ac.kr>
2020-07-29 20:56:12 +0900

    #19532 Accepted!

---
+++ Codes/19500/19532.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e, f;
+    scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+    int det = a * e - b * d;
+    printf("%d %d", (e * c - b * f) / det, (a * f - c * d) / det);
+    return 0;
+}
