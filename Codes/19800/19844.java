86d5102226f41b6f75230283d2127f18487c54f8
calofmijuck <calofmijuck@snu.ac.kr>
2020-10-17 22:25:40 +0900

    #19844 - Fix compile error.

--- Codes/19800/19844.java
+++ Codes/19800/19844.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+class Main {
     public static int ans = 0;
 
     public static void main(String[] args) throws Exception {
