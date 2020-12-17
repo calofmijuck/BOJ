a070f09e671a926568c33ef75b620b36a340c1e5
calofmijuck <calofmijuck@snu.ac.kr>
2020-12-17 09:10:33 +0900

    Fixed #2504

--- Codes/2500/2504.cpp
+++ Codes/2500/2504.cpp
@@ -3,26 +3,33 @@ using namespace std;
 
 int value(string str) {
     int idx, cnt = 1;
-    if(str.size() == 2 || str.size() == 1) {
-        if(str.at(0) == '(') return 2;
-        else return 3;
+    if (str.size() == 2 || str.size() == 1) {
+        if (str.at(0) == '(')
+            return 2;
+        else
+            return 3;
     }
-    if(str.length() == 0) return 0;
-    if(str.at(0) == '(') {
-        for(int i = 1; i < str.size(); ++i) {
-            if(str.at(i) == '(') cnt++;
-            else if(str.at(i) == ')') cnt--;
-            if(cnt == 0) {
+    if (str.length() == 0) 
+        return 0;
+    if (str.at(0) == '(') {
+        for (int i = 1; i < (int)str.size(); ++i) {
+            if (str.at(i) == '(') 
+                cnt++;
+            else if (str.at(i) == ')') 
+                cnt--;
+            if (cnt == 0) {
                 idx = i;
                 break;
             }
         }
         return 2 * (value(str.substr(1, idx - 1)) + !(idx - 1)) + value(str.substr(idx + 1));
     } else {
-        for(int i = 1; i < str.size(); ++i) {
-            if(str.at(i) == '[') cnt++;
-            else if(str.at(i) == ']') cnt--;
-            if(cnt == 0) {
+        for (int i = 1; i < (int)str.size(); ++i) {
+            if (str.at(i) == '[') 
+                cnt++;
+            else if (str.at(i) == ']') 
+                cnt--;
+            if (cnt == 0) {
                 idx = i;
                 break;
             }
@@ -35,23 +42,25 @@ int main() {
     string str;
     stack<char> st;
     cin >> str;
-    for(int i = 0; i < str.size(); ++i) {
+    for (int i = 0; i < (int)str.size(); ++i) {
         char c = str.at(i);
-        if(c == '(' || c == '[') {
+        if (c == '(' || c == '[') {
             st.push(c);
             continue;
-        } else if(c == ')') {
-            while(!st.empty() && st.top() != '(') st.pop();
-        } else if(c == ']') {
-            while(!st.empty() && st.top() != '[') st.pop();
-        }
-        if(st.empty()) {
+        } else if (st.empty()) {
             printf("0");
             return 0;
+        } else {
+            char top = st.top();
+            if ((c == ')' && top == '(') || (c == ']' && top == '[')) {
+                st.pop();
+            } else {
+                printf("0");
+                return 0;
+            }
         }
-        st.pop();
     }
-    if(!st.empty()) {
+    if (!st.empty()) {
         printf("0");
         return 0;
     }
