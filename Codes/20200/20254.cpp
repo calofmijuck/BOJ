bdd68dc4b7d0f579eea5a72d961eb1e13df9f751
Sungchan Yi <calofmijuck@snu.ac.kr>
2021-04-02 23:42:32 +0900

    Solved 20254, 20492 just for fun

---
+++ Codes/20200/20254.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int u_r, t_r, u_o, t_o;
+    cin >> u_r >> t_r >> u_o >> t_o;
+    cout << 56 * u_r + 24 * t_r + 14 * u_o + 6 * t_o;
+}
---
+++ Codes/20400/20492.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    cout << (int) (n * 0.78) << ' ' << (int) (n * 0.8) + (int) (n * 0.2 * 0.78);
+}
