7e587900f47dd8ac66e142f9c3ed1ee4045f49fe
Sungchan Yi <calofmijuck@snu.ac.kr>
2021-04-21 08:04:28 +0900

    feat: compile and run specific problem

--- cmp.sh
+++ cmp.sh
@@ -1,2 +1,14 @@
-g++ submit.cpp -Wall -std=c++17 -o submit
-./submit
+#!/bin/bash
+
+get_dirname () {
+    number=$(($1 / 100 * 100))
+    printf $number
+}
+
+if [ $# -ne 0 ];
+then
+    dirname=$(get_dirname $1)
+    g++ Codes/$dirname/$1.cpp -Wall -std=c++17 -o submit -I . ${@:2} && ./submit
+else
+    g++ submit.cpp -Wall -std=c++17 -o submit && ./submit
+fi
