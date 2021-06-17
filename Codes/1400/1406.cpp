772166b461ddfc0ac685c9373fab68bd36067422
Sungchan Yi <calofmijuck@snu.ac.kr>
2021-06-17 23:37:15 +0900

    feat: #1406 Accepted!

---
+++ Codes/1400/1406.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node {
+public:
+    char c;
+    node *prev;
+    node *next;
+
+    node(const char c) : c(c), prev(0), next(0) { };
+};
+
+node *insert(node *ptr, char c) {
+    node *new_node = new node(c);
+
+    if (!ptr) {
+        return new_node;
+    }
+
+    if (ptr -> next) {
+        ptr -> next -> prev = new_node;
+        new_node -> next = ptr -> next;
+    }
+    ptr -> next = new_node;
+    new_node -> prev = ptr;
+
+    return new_node;
+}
+
+node *remove(node *ptr) {
+    if (ptr -> c == '\0') {
+        return ptr;
+    }
+
+    node *prev = ptr -> prev;
+    if (ptr -> next) {
+        ptr -> next -> prev = ptr -> prev;
+    }
+    prev -> next = ptr -> next;
+    delete ptr;
+
+    return prev;
+}
+
+node *go_left(node *ptr) {
+    if (ptr -> prev) {
+        return ptr -> prev;
+    }
+    return ptr;
+}
+
+node *go_right(node *ptr) {
+    if (ptr -> next) {
+        return ptr -> next;
+    }
+    return ptr;
+}
+
+pair<node *, node *> init(string& s) {
+    node *cursor = new node(0);
+    node *head = cursor;
+    for (int i = 0; i < (int)s.size(); ++i) {
+        cursor = insert(cursor, s[i]);
+    }
+
+    return {head, cursor};
+}
+
+void print(node *head) {
+    head = head -> next;
+    while (head) {
+        cout << (head -> c);
+        head = head -> next;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    string s;
+    cin >> s;
+    auto [head, cursor] = init(s);
+
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; ++i) {
+        string op, c;
+        cin >> op;
+        switch (op[0]) {
+            case 'L':
+                cursor = go_left(cursor);
+                break;
+            case 'D':
+                cursor = go_right(cursor);
+                break;
+            case 'B':
+                cursor = remove(cursor);
+                break;
+            case 'P':
+                cin >> c;
+                cursor = insert(cursor, c[0]);
+                break;
+            default:
+                break;
+        }
+    }
+
+    print(head);
+    return 0;
+}
