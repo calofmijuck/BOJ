23b8d4b0456d23a06befc2cab503cc556b765a81
calofmijuck <calofmijuck@snu.ac.kr>
2021-09-08 22:54:48 +0900

    feat: #6768 Accepted!

---
+++ Codes/6700/6768.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int j;
+    cin >> j;
+    cout << (j - 1) * (j - 2) / 2 * (j - 3) / 3;
+    return 0;
+}
