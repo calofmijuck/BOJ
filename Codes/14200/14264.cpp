45c4f6fc6dda9b5fcf1efb29fa35ce63eb630dc0
calofmijuck <calofmijuck@snu.ac.kr>
2021-09-14 23:10:03 +0900

    feat: #14264 Accepted!

---
+++ Codes/14200/14264.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    double l;
+    cin >> l;
+    cout << fixed << setprecision(10) << l * l * sqrt(3) / 4;
+    return 0;
+}
