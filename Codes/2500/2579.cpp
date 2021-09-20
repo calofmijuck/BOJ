4067588540c385e6bbb94511e6d20555b1684f82
Sungchan Yi <calofmijuck@snu.ac.kr>
2021-09-20 10:42:11 +0900

    refactor: refactored #2579

--- Codes/2500/2579.cpp
+++ Codes/2500/2579.cpp
@@ -1,17 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int st[301], dp[301];
+int max_score[305];
+
+int find_max_score(int n, vector<int>& score) {
+    max_score[1] = score[1];
+    max_score[2] = score[1] + score[2];
+    for (int i = 3; i <= n; ++i) {
+        max_score[i] = max(max_score[i - 2], max_score[i - 3] + score[i - 1]) + score[i];
+    }
+    return max_score[n];
+}
 
 int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     int n;
-    scanf("%d", &n);
-    for(int i = 1; i <= n; ++i) scanf("%d", st + i);
-    dp[1] = st[1];
-    dp[2] = st[1] + st[2];
-    for(int i = 3; i <= n; ++i) {
-        dp[i] = max(dp[i - 2], dp[i - 3] + st[i - 1]) + st[i];
+    cin >> n;
+    vector<int> score(n + 1);
+    for (int i = 1; i <= n; ++i) {
+        cin >> score[i];
     }
-    printf("%d", dp[n]);
+    cout << find_max_score(n, score);
     return 0;
 }
