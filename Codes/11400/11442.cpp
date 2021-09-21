d6b4f5499088f884620fd11a1be2abc27030040d
calofmijuck <calofmijuck@snu.ac.kr>
2021-09-21 11:32:45 +0900

    feat: #11442, #11443 Accepted!

---
+++ Codes/11400/11442.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<vector<int>> Matrix;
+
+const int MOD = 1000000007;
+
+Matrix matmul(Matrix A, Matrix B) {
+    Matrix ret(A.size(), vector<int>(A.size()));
+    for (int i = 0; i < (int) A.size(); ++i) {
+        for (int j = 0; j < (int) B.size(); ++j) {
+            for (int k = 0; k < (int) A.size(); ++k) {
+                ret[i][j] = (ret[i][j] + (ll) A[i][k] * B[k][j]) % MOD;
+            }
+        }
+    }
+    return ret;
+}
+
+Matrix pow(Matrix M, ll exponent) {
+    Matrix result = {{1, 0}, {0, 1}};
+    while (exponent > 0) {
+        if (exponent & 1) {
+            result = matmul(M, result);
+        }
+        exponent >>= 1;
+        M = matmul(M, M);
+    }
+    return result;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+
+    Matrix A = {{1, 1}, {1, 0}};
+
+    if (n % 2 == 1) {
+        n += 1;
+    }
+
+    Matrix result = pow(A, n);
+    cout << result[1][0];
+    return 0;
+}
