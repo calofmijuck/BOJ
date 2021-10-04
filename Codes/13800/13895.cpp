ec8174878f33e4073c0a1aeaf383f13f1b5dbae6
calofmijuck <calofmijuck@snu.ac.kr>
2021-10-04 22:40:38 +0900

    feat: #13895 Accepted!

---
+++ Codes/13800/13895.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    scanf("%d + %d = %d", &a, &b, &c);
+    if (a + b == c) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+    return 0;
+}
