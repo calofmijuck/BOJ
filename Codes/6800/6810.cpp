44035c64092207b635f4f1f666c04d5ba0aad8fa
calofmijuck <calofmijuck@snu.ac.kr>
2021-12-11 23:44:37 +0900

    feat: solved easy problems

---
+++ Codes/6800/6810.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << "The 1-3-sum is " << 91 + a * 1 + b * 3 + c * 1;
+    return 0;
+}
