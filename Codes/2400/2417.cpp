1e1c833192148621cc4c331d41b44c6e29758728
Sungchan Yi <calofmijuck@snu.ac.kr>
2022-05-14 00:28:23 +0900

    feat: Re-solve #2417

---
+++ Codes/2400/2417.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    long long n;
+    cin >> n;
+    cout << (long long) ceil(sqrt((long double) n));
+    return 0;
+}
