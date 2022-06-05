dce5250b092bc1d5c18992bc7394347875449887
Sungchan Yi <calofmijuck@snu.ac.kr>
2022-06-05 12:49:13 +0900

    refactor: #7677 using matrix multiply method

--- Codes/7600/7677.cpp
+++ Codes/7600/7677.cpp
@@ -1,35 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MOD 10000
+
+const int MOD = 1e4;
+
+using matrix = vector<vector<int>>;
+
+matrix multiply(const matrix& a, const matrix& b) {
+    matrix ret(2, vector<int>(2));
+    ret[0][0] = (a[0][0] * b[0][0] + a[0][1] * b[1][0]) % MOD;
+    ret[0][1] = (a[0][0] * b[0][1] + a[0][1] * b[1][1]) % MOD;
+    ret[1][0] = (a[1][0] * b[0][0] + a[1][1] * b[1][0]) % MOD;
+    ret[1][1] = (a[1][0] * b[0][1] + a[1][1] * b[1][1]) % MOD;
+    return ret;
+}
 
 int fib(int n) {
-    int A[2][2] = {1, 1, 1, 0}, B[2][2] = {1, 0, 0, 1}, tmp[2][2];
+    matrix A = { {1, 1}, {1, 0} };
+    matrix B = { {1, 0}, {0, 1} };
     int exp = n;
-    while(exp > 0) {
-        if(exp & 1) {
-            tmp[0][0] = (A[0][0] * B[0][0] + A[0][1] * B[1][0]) % MOD;
-            tmp[0][1] = (A[0][0] * B[0][1] + A[0][1] * B[1][1]) % MOD;
-            tmp[1][0] = (A[1][0] * B[0][0] + A[1][1] * B[1][0]) % MOD;
-            tmp[1][1] = (A[1][0] * B[0][1] + A[1][1] * B[1][1]) % MOD;
-            for(int i = 0; i < 2; ++i) {
-                for(int j = 0; j < 2; ++j) B[i][j] = tmp[i][j];
-            }
+    while (exp > 0) {
+        if (exp & 1) {
+            B = multiply(A, B);
         }
         exp >>= 1;
-        tmp[0][0] = (A[0][0] * A[0][0] + A[0][1] * A[1][0]) % MOD;
-        tmp[0][1] = (A[0][0] * A[0][1] + A[0][1] * A[1][1]) % MOD;
-        tmp[1][0] = (A[1][0] * A[0][0] + A[1][1] * A[1][0]) % MOD;
-        tmp[1][1] = (A[1][0] * A[0][1] + A[1][1] * A[1][1]) % MOD;
-        for(int i = 0; i < 2; ++i) {
-            for(int j = 0; j < 2; ++j) A[i][j] = tmp[i][j];
-        }
+        A = multiply(A, A);
     }
     return B[1][0] % MOD;
 }
 
 int main() {
     int n;
-    while(scanf("%d", &n) && (n + 1)) {
+    while (scanf("%d", &n) && (n + 1)) {
         printf("%d\n", fib(n));
     }
     return 0;
