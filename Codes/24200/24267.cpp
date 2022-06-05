2b6ae6b289df410d33f6da7b223d12ff80e31fc1
Sungchan Yi <calofmijuck@snu.ac.kr>
2022-06-05 11:59:07 +0900

    feat: solved algorithm running time problems

---
+++ Codes/24200/24267.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    cout << (long long) n * (n - 1) / 2 * (n - 2) / 3 << '\n' << 3;
+    return 0;
+}
