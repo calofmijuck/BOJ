10919aa81eaba9c1871a2663a80b3d641e7fe99c
Sungchan Yi <calofmijuck@snu.ac.kr>
2022-06-06 13:03:33 +0900

    refactor: refactor FFT implementation

--- Algorithms/FFT-Cooley-Tukey.cpp
+++ Algorithms/FFT-Cooley-Tukey.cpp
@@ -3,68 +3,67 @@
 #include <complex>
 #include <iostream>
 #include <valarray>
-
 using namespace std;
 
-typedef complex<double> Complex;
-typedef valarray<Complex> CArray;
+using base = complex<double>;
+using base_array = valarray<base>;
 
-// Cooley - Tukey FFT
-void fft(CArray& x) {
+// Cooley-Tukey FFT
+void fft(base_array& x) {
     size_t N = x.size();
-    if(N <= 1) {
+    if (N <= 1) {
         return;
     }
 
     // Divide
-    CArray even = x[slice(0, N / 2, 2)];
-    CArray odd = x[slice(1, N / 2, 2)];
+    base_array even = x[slice(0, N / 2, 2)];
+    base_array odd = x[slice(1, N / 2, 2)];
 
     // Conquer
     fft(even);
     fft(odd);
 
     // Merge
-    for(size_t k = 0; k < N / 2; ++k) {
-        Complex t = polar(1.0, -2 * M_PI * k / N) * odd[k];
+    for (size_t k = 0; k < N / 2; ++k) {
+        base t = polar(1.0, -2 * M_PI * k / N) * odd[k];
         x[k] = even[k] + t;
         x[k + N / 2] = even[k] - t;
     }
 }
 
-void ifft(CArray& x) {
+void ifft(base_array& x) {
     x = x.apply(conj);
     fft(x);
     x = x.apply(conj);
     x /= x.size();
 }
 
-void multiply(CArray& a, CArray& b, valarray<int>& res) {
-    CArray fa(a), fb(b);
+void multiply(base_array& a, base_array& b, valarray<int>& res) {
+    base_array fa(a), fb(b);
     int n = fa.size();
     fft(fa); fft(fb);
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         fa[i] *= fb[i];
     }
     ifft(fa);
     res.resize(n);
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         res[i] = (int) (fa[i].real() + (fa[i].real() > 0 ? 0.5 : -0.5));
     }
 }
 
 int main() {
-    Complex arr[] = {1.0, 1.0, 1.0, 1.0, 0, 0, 0, 0};
-    CArray data(arr, 8);
+    base arr[] = { 1.0, 1.0, 1.0, 1.0, 0, 0, 0, 0 };
+    base_array data(arr, 8);
 
     fft(data);
-    for(int i = 0; i < data.size(); i++) {
+    for (int i = 0; i < data.size(); i++) {
         cout << data[i] << ' ';
     }
     puts("");
 
     ifft(data);
-    for(int i = 0; i < data.size(); i++) {
+    for (int i = 0; i < data.size(); i++) {
         cout << data[i] << ' ';
     }
     puts("");
--- Algorithms/FFT-Iterative.cpp
+++ Algorithms/FFT-Iterative.cpp
@@ -1,47 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef complex<double> Complex;
+using base = complex<double>;
 
-void fft(vector<Complex> &a, bool inv) {
+void fft(vector<base>& a, bool inv) {
     int n = a.size();
-    for(int i = 1, j = 0; i < n; ++i) {
+    for (int i = 1, j = 0; i < n; ++i) {
         int bit = n >> 1;
-        for(; j >= bit; bit >>= 1) 
+        for (; j >= bit; bit >>= 1)
             j -= bit;
         j += bit;
-        if(i < j) swap(a[i], a[j]);
+        if (i < j) swap(a[i], a[j]);
     }
-    for(int len = 2; len <= n; len <<= 1) {
+    for (int len = 2; len <= n; len <<= 1) {
         double ang = 2 * acos(-1) / len * (inv ? -1 : 1);
-        Complex wlen(cos(ang), sin(ang));
-        for(int i = 0; i < n; i += len) {
-            Complex w(1);
-            for(int j = 0; j < len / 2; ++j) {
-                Complex u = a[i + j], v = a[i + j + len / 2] * w;
+        base wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            base w(1);
+            for (int j = 0; j < len / 2; ++j) {
+                base u = a[i + j], v = a[i + j + len / 2] * w;
                 a[i + j] = u + v;
                 a[i + j + len / 2] = u - v;
                 w *= wlen;
             }
         }
     }
-    if(inv) {
-        for(int i = 0; i < n; i++) 
+    if (inv) {
+        for (int i = 0; i < n; i++)
             a[i] /= n;
     }
 }
 
-void multiply(const vector<int> &a, const vector<int> &b, vector<int> &res) {
-    vector<Complex> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+void multiply(const vector<int>& a, const vector<int>& b, vector<int>& res) {
+    vector<base> fa(a.begin(), a.end()), fb(b.begin(), b.end());
     int n = 1;
-    while(n < max(a.size(), b.size()))
+    while (n < max(a.size(), b.size()))
         n <<= 1;
     n <<= 1;
     fa.resize(n); fb.resize(n); res.resize(n);
     fft(fa, false); fft(fb, false);
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         fa[i] *= fb[i];
     fft(fa, true);
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         res[i] = int(fa[i].real() + (fa[i].real() > 0 ? 0.5 : -0.5));
 }
