9fdfba734605ab291641951ab6dfeea9f29384da
Sungchan Yi <calofmijuck@snu.ac.kr>
2022-06-27 10:09:16 +0900

    feat: solve 2 problems in Bronze 4

---
+++ Codes/20800/20833.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    int x = (n + 1) * n / 2;
+    cout << x * x;
+    return 0;
+}
