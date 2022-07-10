627a7f07c5b62f9e71653b9a4c52abc173681df6
Sungchan Yi <calofmijuck@snu.ac.kr>
2022-07-10 11:25:01 +0900

    feat: #4395 Accepted!

---
+++ Codes/4300/4395.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int calculate(int d) {
+    if (d == 0) {
+        return 0;
+    }
+
+    int k = int(ceil(sqrt(d))) - 1;
+    if (k * (k + 1) < d) {
+        return 2 * k + 1;
+    } else {
+        return 2 * k;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+
+        cout << calculate(y - x) << '\n';
+    }
+    return 0;
+}
