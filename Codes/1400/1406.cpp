7b6a1e01fa6cda04cc23964a90c5c455295cc8c5
Sungchan Yi <calofmijuck@snu.ac.kr>
2022-07-28 23:27:59 +0900

    refactor: #1406

--- Codes/1400/1406.cpp
+++ Codes/1400/1406.cpp
@@ -4,81 +4,31 @@ using namespace std;
 class node {
 public:
     char c;
-    node *prev;
-    node *next;
+    node* prev;
+    node* next;
 
     node(const char c) : c(c), prev(0), next(0) { };
 };
 
-node *insert(node *ptr, char c) {
-    node *new_node = new node(c);
+node* insert(node*, char);
 
-    if (!ptr) {
-        return new_node;
-    }
+node* remove(node*);
 
-    if (ptr -> next) {
-        ptr -> next -> prev = new_node;
-        new_node -> next = ptr -> next;
-    }
-    ptr -> next = new_node;
-    new_node -> prev = ptr;
+node* go_left(node*);
 
-    return new_node;
-}
+node* go_right(node*);
 
-node *remove(node *ptr) {
-    if (ptr -> c == '\0') {
-        return ptr;
-    }
+pair<node*, node*> initialize_string(string& s);
 
-    node *prev = ptr -> prev;
-    if (ptr -> next) {
-        ptr -> next -> prev = ptr -> prev;
-    }
-    prev -> next = ptr -> next;
-    delete ptr;
-
-    return prev;
-}
-
-node *go_left(node *ptr) {
-    if (ptr -> prev) {
-        return ptr -> prev;
-    }
-    return ptr;
-}
-
-node *go_right(node *ptr) {
-    if (ptr -> next) {
-        return ptr -> next;
-    }
-    return ptr;
-}
-
-pair<node *, node *> init(string& s) {
-    node *cursor = new node(0);
-    node *head = cursor;
-    for (int i = 0; i < (int)s.size(); ++i) {
-        cursor = insert(cursor, s[i]);
-    }
-
-    return {head, cursor};
-}
-
-void print(node *head) {
-    head = head -> next;
-    while (head) {
-        cout << (head -> c);
-        head = head -> next;
-    }
-}
+void print_string(node*);
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
     string s;
     cin >> s;
-    auto [head, cursor] = init(s);
+
+    auto [head, cursor] = initialize_string(s);
 
     int m;
     cin >> m;
@@ -104,6 +54,70 @@ int main() {
         }
     }
 
-    print(head);
+    print_string(head);
     return 0;
+}
+
+node* insert(node* ptr, char c) {
+    node* new_node = new node(c);
+
+    if (!ptr) {
+        return new_node;
+    }
+
+    if (ptr -> next) {
+        ptr -> next -> prev = new_node;
+        new_node -> next = ptr -> next;
+    }
+    ptr -> next = new_node;
+    new_node -> prev = ptr;
+
+    return new_node;
+}
+
+node* remove(node* ptr) {
+    if (ptr -> c == '\0') {
+        return ptr;
+    }
+
+    node* prev = ptr -> prev;
+    if (ptr -> next) {
+        ptr -> next -> prev = ptr -> prev;
+    }
+    prev -> next = ptr -> next;
+    delete ptr;
+
+    return prev;
+}
+
+node* go_left(node* ptr) {
+    if (ptr -> prev) {
+        return ptr -> prev;
+    }
+    return ptr;
+}
+
+node* go_right(node* ptr) {
+    if (ptr -> next) {
+        return ptr -> next;
+    }
+    return ptr;
+}
+
+pair<node*, node*> initialize_string(string& s) {
+    node* cursor = new node(0);
+    node* head = cursor;
+    for (int i = 0; i < (int) s.size(); ++i) {
+        cursor = insert(cursor, s[i]);
+    }
+
+    return { head, cursor };
+}
+
+void print_string(node* head) {
+    head = head -> next;
+    while (head) {
+        cout << (head -> c);
+        head = head -> next;
+    }
 }
