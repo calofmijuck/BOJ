220136ebcedff66ada517b457895e47e0e23773c
Sungchan Yi <calofmijuck@snu.ac.kr>
2022-09-26 22:45:39 +0900

    feat: #10480 Accepted!

---
+++ Codes/10400/10480.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int x;
+        cin >> x;
+
+        if (x % 2 == 0) {
+            cout << x << " is even\n";
+        } else {
+            cout << x << " is odd\n";
+        }
+    }
+    return 0;
+}
