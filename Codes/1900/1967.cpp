ed0dcfdbfcdedc21d72fd8c0f346585cff8b0e3a
Sungchan Yi <calofmijuck@snu.ac.kr>
2026-01-22 11:43:54 +0900

    refactor: method name for diameter of tree

--- Codes/1900/1967.cpp
+++ Codes/1900/1967.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> pii;
 
-vector<pii> adj[10101];
+vector<pair<int, int>> adj[10101];
 int ans;
 
-int radius(int v) {
+int max_depth(int v) {
     int max1 = 0, max2 = 0;
-    for(int i = 0; i < adj[v].size(); ++i) {
-        pii p = adj[v][i];
-        int curr = radius(p.first) + p.second;
-        if(curr > max1) {
+    for (int i = 0; i < (int) adj[v].size(); ++i) {
+        auto [next, weight] = adj[v][i];
+        int curr = max_depth(next) + weight;
+        if (curr > max1) {
             max2 = max1;
             max1 = curr;
-        } else if(curr > max2) max2 = curr;
+        } else if (curr > max2) {
+            max2 = curr;
+        }
     }
     ans = max(ans, max1 + max2);
     return max1;
@@ -22,10 +23,10 @@ int radius(int v) {
 int main() {
     int n, p, c, w;
     scanf("%d", &n);
-    for(int i = 0; i < n - 1; ++i) {
+    for (int i = 0; i < n - 1; ++i) {
         scanf("%d %d %d", &p, &c, &w);
-        adj[p].push_back({c, w});
+        adj[p].push_back({ c, w });
     }
-    radius(1);
+    max_depth(1);
     printf("%d", ans);
 }
